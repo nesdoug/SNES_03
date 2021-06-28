9f862078d03189c7718dedb92305ecfc2fe38feb
nesdoug <dougfraker@gmail.com>
2021-06-28 16:36:45 -0400

    update 2.0
    update 2.0

--- mainB.asm
+++ mainB.asm
@@ -3,7 +3,8 @@
 .p816
 .smart
 
-.include "defines.asm"
+.include "regs.asm"
+.include "variables.asm"
 .include "macros.asm"
 .include "init.asm"
 .include "unrle.asm"
@@ -14,8 +15,8 @@
 .segment "CODE"
 
 ; enters here in forced blank
-main:
-.a16 ; just a standardized setting from init code
+Main:
+.a16 ; the setting from init code
 .i16
 	phk
 	plb
@@ -24,11 +25,11 @@ main:
 	
 ; DMA from BG_Palette to CGRAM
 	A8
-	stz $2121 ; $2121 cg address = zero
+	stz CGADD ; $2121 cgram address = zero
 	
 	stz $4300 ; transfer mode 0 = 1 register write once
 	lda #$22  ; $2122
-	sta $4301 ; destination, pal data
+	sta $4301 ; destination, cgram data
 	ldx #.loword(BG_Palette)
 	stx $4302 ; source
 	lda #^BG_Palette
@@ -36,82 +37,101 @@ main:
 	ldx #32
 	stx $4305 ; length
 	lda #1
-	sta $420b ; start dma, channel 0
+	sta MDMAEN ; $420b start dma, channel 0
 	
 	
 ; DMA from Tiles to VRAM	
 	lda #V_INC_1 ; the value $80
-	sta vram_inc ; $2115 = set the increment mode +1
-	ldx #$0000
-	stx vram_addr ; set an address in the vram of $0000
-	
-	lda #1
-	sta $4300 ; transfer mode, 2 registers 1 write
-			  ; $2118 and $2119 are a pair Low/High
-	lda #$18  ; $2118
-	sta $4301 ; destination, vram data
+	sta VMAIN ; $2115 = set the increment mode +1
 
-; decompress first
-	AXY16
-	lda #.loword(Tiles)
-	ldx #^Tiles
-	jsl unrle ; unpacks to 7f0000 UNPACK_ADR
-	; returns y = length
-	; ax = unpack address (x is bank)
-	sta $4302 ; source
-	txa
-	A8
-	sta $4304 ; bank
-	sty $4305 ; length
-	lda #1
-	sta $420b ; start dma, channel 0
+
+	ldx #$0000
+	stx VMADDL ; set an address in the vram of $0000
+	
+; decompress Tiles to VRAM
+	UNPACK_TO_VRAM Tiles
 	
 	
 	
 ; DMA from Tilemap to VRAM	
 	ldx #$6000
-	stx vram_addr ; set an address in the vram of $6000
+	stx VMADDL ; set an address in the vram of $6000
 	
-; decompress first
-	AXY16
-	lda #.loword(Tilemap)
-	ldx #^Tilemap
-	jsl unrle ; unpacks to 7f0000 UNPACK_ADR
-	; returns y = length
-	; ax = unpack address (x is bank)
-	sta $4302 ; source
-	txa
-	A8
-	sta $4304 ; bank
-	sty $4305 ; length
-	lda #1
-	sta $420b ; start dma, channel 0
+; decompress
+	UNPACK_TO_VRAM Tilemap
 	
 	
 ; a is still 8 bit.
 	lda #1 ; mode 1, tilesize 8x8 all
-	sta bg_size_mode ; $2105
+	sta BGMODE ; $2105
 	
 ; 210b = tilesets for bg 1 and bg 2
 ; (210c for bg 3 and bg 4)
 ; steps of $1000 -321-321... bg2 bg1
-	stz bg12_tiles ; $210b BG 1 and 2 TILES at VRAM address $0000
+	stz BG12NBA ; $210b BG 1 and 2 TILES at VRAM address $0000
 	
 	; 2107 map address bg 1, steps of $400, but -54321yx
 	; y/x = map size... 0,0 = 32x32 tiles
 	; $6000 / $100 = $60
 	lda #$60 ; address $6000
-	sta tilemap1 ; $2107
+	sta BG1SC ; $2107
 
 	lda #BG1_ON	; only bg 1 is active
-	sta main_screen ; $212c
+	sta TM ; $212c
 	
 	lda #FULL_BRIGHT ; $0f = turn the screen on (end forced blank)
-	sta fb_bright ; $2100
+	sta INIDISP ; $2100
 
 
-InfiniteLoop:	
-	jmp InfiniteLoop
+Infinite_Loop:	
+	A8
+	XY16
+	jsr Wait_NMI
+	
+	;code goes here
+
+	jmp Infinite_Loop
+	
+	
+	
+Wait_NMI:
+.a8
+.i16
+;should work fine regardless of size of A
+	lda in_nmi ;load A register with previous in_nmi
+@check_again:	
+	WAI ;wait for an interrupt
+	cmp in_nmi	;compare A to current in_nmi
+				;wait for it to change
+				;make sure it was an nmi interrupt
+	beq @check_again
+	rts
+	
+	
+	
+;jsl here	
+DMA_VRAM:
+.a16
+.i16
+; do during forced blank	
+; first set VRAM_Addr and VRAM_Inc
+; a = source
+; x = source bank
+; y = length in bytes
+	php
+	rep #$30 ;axy16
+	sta $4302 ; source and 4303
+	sep #$20 ;a8
+	txa
+	sta $4304 ; bank
+	lda #$18
+	sta $4301 ; destination, vram data
+	sty $4305 ; length, and 4306
+	lda #1
+	sta $4300 ; transfer mode, 2 registers, write once = 2 bytes
+	sta $420b ; start dma, channel 0
+	plp
+	rtl	
 	
 	
 	
